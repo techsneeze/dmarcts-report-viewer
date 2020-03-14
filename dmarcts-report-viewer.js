080beb22f7f84474b5a5b6adaf9ebc7445eb90cd
Franck MAHE <mahe@civis.net>
2020-03-14 14:02:46 +0100

    Add files for Style & JS

---
+++ dmarcts-report-viewer.js
@@ -0,0 +1,48 @@
+function reportData(id, url) {
+  $.ajax({
+    type: 'GET',
+    url: url,
+    timeout: 10000,
+    success: function(data) {
+      $('#reportData').show();
+      $("#reportData").html(data);
+      if (id != -1) {
+        $('#'+id).css("background-color", "lightgrey");
+      }
+    },
+    error: function (XMLHttpRequest, textStatus, errorThrown) {
+      $("#reportData").html('Timeout contacting server..');
+    }
+  });
+}
+
+$(document).ready(function(){
+  $(".close").click(function(){
+    $("#reportData").hide();
+  });
+});
+
+function printData() {
+  var divToPrint=document.getElementById("reportData");
+  newWin= window.open("");
+  newWin.document.write(divToPrint.innerHTML);
+  setTimeout(function(){
+    newWin.print();
+    newWin.close();
+  }, 10);
+}
+
+function showXML(id) {
+  $.ajax({
+    type: 'GET',
+    url: 'https://cert.civis.net/dmarc-report/index.php?showxml='+id,
+    timeout: 10000,
+    success: function(data) {
+      $('#showxml').show();
+      $("#showxml").html(data);
+    },
+    error: function (XMLHttpRequest, textStatus, errorThrown) {
+      $("#showxml").html('Timeout contacting server..');
+    }
+  });
+}
