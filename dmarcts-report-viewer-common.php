f169185cccb9dbf50a3a7562c72946b1cad325c6
Daniel Caillibaud <daniel.caillibaud@sesamath.net>
2021-01-27 17:04:55 +0100

    set red color if reject or quarantine

--- dmarcts-report-viewer-common.php
+++ dmarcts-report-viewer-common.php
@@ -45,7 +45,10 @@ function main() {
 function get_status_color($row) {
 	$status = "";
 	$status_num = "";
-    if (($row['dkimresult'] == "fail") && ($row['spfresult'] == "fail")) {
+    if (($row['disposition'] == "reject") || ($row['disposition'] == "quarantine")) {
+      $status="red";
+      $status_num="4";
+    } elseif (($row['dkimresult'] == "fail") && ($row['spfresult'] == "fail")) {
 	    $status="red";
 		$status_num="4";
     } elseif (($row['dkimresult'] == "fail") || ($row['spfresult'] == "fail") || ($row['dkim_align'] == "fail") || ($row['spf_align'] == "fail")) {
